bdb632d9747dd42e9afebde8d7723cd32e8b4151
Derek <derekdanielson84@gmail.com>
2023-09-26 13:52:37 -0600

    Fixed details div

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ async function getWeatherData(cityValue){
         weatherDataEl.querySelector('.icon').innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather icon">`;
         weatherDataEl.querySelector('.temperature').innerHTML = `${temperature}°F`;
         weatherDataEl.querySelector('.description').innerHTML = description;
-        weatherDataEl.querySelector('.details').innerHTML = details.map((detail)=> `<div>${detail}<div>`).join('');
+        weatherDataEl.querySelector('.details').innerHTML = details.map((detail)=> `<div>${detail}</div>`).join('');
 
     } catch (error) {
         
